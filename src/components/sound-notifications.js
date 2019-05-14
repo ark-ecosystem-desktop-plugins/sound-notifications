1539d2c72bb0db7ba6573a61dd8ea56e37f907f6
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-05-14 14:51:07 -0300

    fix: move listener to the main file

--- src/components/sound-notifications.js
+++ src/components/sound-notifications.js
@@ -1,29 +1,3 @@
-const audioContext = new AudioContext()
-
-// Load audio buffer
-const mediaFile = require('../assets/1-up.json')
-const mediaBuffer = new Float32Array(Array.from(Object.values(mediaFile)))
-const audioBuffer = audioContext.createBuffer(1, mediaBuffer.byteLength, 22050)
-audioBuffer.getChannelData(0).set(mediaBuffer)
-
-const play = () => {
-  const audioSource = audioContext.createBufferSource()
-  audioSource.buffer = audioBuffer
-  audioSource.connect(audioContext.destination)
-  audioSource.start()
-}
-
-const onNewTransactions = () => {
-  walletApi.eventBus.onAny(eventName => {
-    if (eventName.includes('transaction:new')) {
-      play()
-    }
-  })
-}
-
-// Call this when starting the application
-onNewTransactions()
-
 module.exports = {
   template: '<div></div>'
 }
